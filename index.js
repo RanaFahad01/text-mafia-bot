540c4fb92bbe57b1b0602e31960cb8a2f6f4ff71
Rana Fahad <ranafahadbh@gmail.com>
2024-11-27 03:10:09 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+//Make your own config/config.json file and add the token there
+//config.json is in the .gitignore so that we don't push the token to the repo by accident
+const { token } = require("config/config.json");
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
+});
+
+client.on('ready', (client) => {
+    console.log(`${client.user.username} is online.`);
+});
+
+client.login(process.env.TOKEN);
