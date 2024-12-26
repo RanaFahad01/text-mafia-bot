ea3541d71687ad4c80e21566b6217a3596c7f070
Rana Fahad <ranafahadbh@gmail.com>
2024-12-26 05:15:09 +0100

    Simple directory changes and switched from config.json to .env approach for config

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-//Make your own config/config.json file and add the token there
-//config.json is in the .gitignore so that we don't push the token to the repo by accident
-const { token } = require("config/config.json");
-const { Client, GatewayIntentBits } = require('discord.js');
+//Make your own src/.env file and add the token there
+//The .env file is in the .gitignore so that the developer doesn't push the token to the repo by accident
+require('dotenv').config();
+const { Client,
+    GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
     intents: [
@@ -16,4 +17,4 @@ client.on('ready', (client) => {
     console.log(`${client.user.username} is online.`);
 });
 
-client.login(token);
+client.login(process.env.TOKEN);
